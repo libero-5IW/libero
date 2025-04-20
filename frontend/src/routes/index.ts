06eff140f5c52aae96b46306f965ed0a9582b35d
Sarah Salamani <sarahsalamani@Sarahs-MacBook-Air.local>
2025-04-20 16:12:03 +0200

    front contract template

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -1,10 +1,13 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import quoteTemplateRoutes from '@/routes/quoteTemplate'
 import securityRoutes from '@/routes/security'
+import contractTemplateRoutes from './contractTemplate' 
+
 
 const routes: Array<RouteRecordRaw> = [
   ...quoteTemplateRoutes,
   ...securityRoutes,
+  ...contractTemplateRoutes,
 ]
 
 const router = createRouter({
