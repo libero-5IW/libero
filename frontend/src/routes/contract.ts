05aa781195656c9693c59d09134d898060542794
Sarah Salamani <sarahsalamani@sarahs-air.home>
2025-06-22 17:06:55 +0200

    routes

---
+++ frontend/src/routes/contract.ts
@@ -0,0 +1,15 @@
+import type { RouteRecordRaw } from 'vue-router'
+import ContractForm from '@/views/contract/ContractForm.vue'
+import ContractList from '@/views/contract/ContractList.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/contract',
+    children: [
+      { path: '', name: 'ContractList', component: ContractList },
+      { path: 'new', name: 'ContractForm', component: ContractForm },
+    ]
+  }
+]
+
+export default routes
