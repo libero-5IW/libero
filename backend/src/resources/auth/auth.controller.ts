d74a03821097d0f07733136e12da8c5335ee69ed
Jay Bury <buryj15@gmail.com>
2025-06-26 11:38:22 +0200

    éviter de monter les node_modules locales dans le container

--- backend/src/resources/auth/auth.controller.ts
+++ backend/src/resources/auth/auth.controller.ts
@@ -5,7 +5,6 @@ import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { JwtPayload } from './interfaces/jwt-payload.interface';
 import { Public } from './decorators/public.decorator';
 import { RegisterDto } from './dto/register.dto';
-import { TwoFaAuthGuard } from './guards/twofa-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -33,28 +32,5 @@ export class AuthController {
       userId: user.userId,
       email: user.email,
     };
-  }
-
-    @Post('twofa/enable')
-  async enableTwoFa(@Req() req: Request) {
-    return this.authService.enableTwoFa(req.user.email);
-  }
-
-  @Post('twofa/disable')
-  @UseGuards(TwoFaAuthGuard)
-  async disableTwoFa(@Req() req: Request) {
-    return this.authService.disableTwoFa(req.user.email);
-  }
-
-  @Post('twofa/verify')
-  @UseGuards(TwoFaAuthGuard)
-  async verifyTwoFa(@Req() req: Request) {
-    return this.authService.verifyTwoFa(req.user.email, req.body.token);
-  }
-
-  @Get('twofa/secret')
-  @UseGuards(TwoFaAuthGuard)
-  async getTwoFaSecret(@Req() req: Request) {
-    return this.authService.getTwoFaSecret(req.user.email);
   }
 }
