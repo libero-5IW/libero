6f5d54cb179a8732172f29bc4eca7a5ec69b65e0
racha <rachaafaf15@gmail.com>
2025-07-08 12:57:38 +0200

    test update code

--- frontend/src/components/Auth/LoginForm.vue
+++ frontend/src/components/Auth/LoginForm.vue
@@ -10,7 +10,7 @@
           elevation="0"
       >
         <template v-slot:title>
-          <span class="font-weight-black">Connexionnh</span>
+          <span class="font-weight-black">Connexion</span>
         </template>
 
         <v-card-text>
