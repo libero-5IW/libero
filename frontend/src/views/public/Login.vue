dc58ddb29937b85cc2fd468bbe9ed4320a50ec97
Sarah Salamani <sarahsalamani@sarahs-air.home>
2025-07-10 01:10:00 +0200

    access login

--- frontend/src/views/public/Login.vue
+++ frontend/src/views/public/Login.vue
@@ -1,30 +1,31 @@
-<template >
-   <CenteredContainer>
-      <LoginForm :form="form" :loading="loading" @submit="handleLogin">
-        <template v-slot:header>
-          <div class="flex justify-center py-4">
-            <div>
-              <v-img :src="logo" width="140"></v-img>
-            </div>
+<template>
+  <CenteredContainer>
+    <LoginForm :form="form" :loading="loading" @submit="handleLogin">
+      <template v-slot:header>
+        <div class="flex justify-center py-4">
+          <div>
+            <v-img :src="logo" width="140" alt="Logo de l'application" />
           </div>
-        </template>
-        <template v-slot:content.additional>
-          <div class="text-center">
-            <p class="text-gray-600">Pas encore de compte ?</p>
-            <router-link to="/register">S'inscrire</router-link>
-          </div>
-          <div class="text-center">
-            <p class="text-gray-600">Mot de passe oublié ?</p>
-            <router-link to="/email-reset-password">Réinitialiser</router-link>
-          </div>
-        </template>
-      </LoginForm>
-    </CenteredContainer>
+        </div>
+      </template>
+
+      <template v-slot:content.additional>
+        <div class="text-center">
+          <p class="text-gray-600">Pas encore de compte ?</p>
+          <router-link to="/register">S'inscrire</router-link>
+        </div>
+        <div class="text-center">
+          <p class="text-gray-600">Mot de passe oublié ?</p>
+          <router-link to="/email-reset-password">Réinitialiser</router-link>
+        </div>
+      </template>
+    </LoginForm>
+  </CenteredContainer>
 </template>
 
 <script setup lang="ts">
   import LoginForm from '@/components/Auth/LoginForm.vue';
-  import CenteredContainer from "@/components/Container/CenteredContainer.vue";
+  import CenteredContainer from '@/components/Container/CenteredContainer.vue';
   import logo from '@/assets/logo.png';
   import { computed, onMounted, reactive } from 'vue';
   import { useToastHandler } from '@/composables/useToastHandler';
@@ -41,7 +42,7 @@
   onMounted(async () => {
     const status = history.state?.toastStatus as ToastStatus;
     const message = history.state?.toastMessage as string;
-  
+
     if (message && status) {
       showToast(status, message);
     }
