4c914f11b9d3f0cda9e3285bec63f8a1348301ee
racha <rachaafaf15@gmail.com>
2025-07-13 17:17:06 +0200

    debug entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-
 set -e
 
-echo "[INFO] Injecting secrets from /run/secrets into environment..."
+echo "[entrypoint] Injecting secrets..."
 
-for secret in /run/secrets/*; do
-  [ -f "$secret" ] || continue
-  VAR_NAME=$(basename "$secret")
-  export "$VAR_NAME"="$(cat "$secret")"
+for file in /run/secrets/*; do
+  [ -f "$file" ] || continue
+  key=$(basename "$file")
+  value=$(cat "$file")
+  export "$key"="$value"
+  echo "[entrypoint] Exported $key"
 done
 
-echo "[INFO] Starting application..."
+echo "[entrypoint] Starting app with JWT_SECRET=${JWT_SECRET:0:4}..."  # Debug: affiche les 4 1ers chars
+
 exec npm run start:prod
